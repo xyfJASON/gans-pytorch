Refactor[WIP]: wgan, wgan-gp; Fix ddp training by adding broadcast_buffers=False

losses/vanilla_gan_loss.py
@@ -17,13 +17,17 @@ class VanillaGANLoss(nn.Module):
         and Yoshua Bengio. “Generative adversarial nets.” Advances in neural information processing systems 27 (2014).
 
     """
-    def __init__(self):
+    def __init__(self, discriminator: nn.Module):
         super().__init__()
+        self.discriminator = discriminator
         self.bce_with_logits = nn.BCEWithLogitsLoss()
 
-    def forward_G(self, fakescore: Tensor):
-        return self.bce_with_logits(fakescore, torch.ones_like(fakescore))
+    def forward_G(self, fake_data: Tensor):
+        fake_score = self.discriminator(fake_data)
+        return self.bce_with_logits(fake_score, torch.ones_like(fake_score))
 
-    def forward_D(self, fakescore: Tensor, realscore: Tensor):
-        return (self.bce_with_logits(fakescore, torch.zeros_like(fakescore)) +
-                self.bce_with_logits(realscore, torch.ones_like(realscore))) / 2
+    def forward_D(self, fake_data: Tensor, real_data: Tensor):
+        fake_score = self.discriminator(fake_data.detach())
+        real_score = self.discriminator(real_data)
+        return (self.bce_with_logits(fake_score, torch.zeros_like(fake_score)) +
+                self.bce_with_logits(real_score, torch.ones_like(real_score))) / 2

models/simple_cnn.py
@@ -97,7 +97,7 @@ class Discriminator(nn.Module):
         self.last_conv = nn.Sequential(
             nn.BatchNorm2d(dim * dim_mults[-1]) if with_bn else nn.Identity(),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(dim * dim_mults[-1], 1, (4, 4), stride=(1, 1), padding=0),
+            nn.Conv2d(dim * dim_mults[-1], 1, (4, 4), stride=1, padding=0),
         )
 
         self.apply(init_weights(init_type))

train_dcgan.py
@@ -36,7 +36,8 @@ def get_parser():
 
 def train(args, cfg):
     # INITIALIZE ACCELERATOR
-    accelerator = accelerate.Accelerator()
+    ddp_kwargs = accelerate.DistributedDataParallelKwargs(broadcast_buffers=False)
+    accelerator = accelerate.Accelerator(kwargs_handlers=[ddp_kwargs])
     device = accelerator.device
     print(f'Process {accelerator.process_index} using device: {device}')
     accelerator.wait_for_everyone()
@@ -149,7 +150,7 @@ def train(args, cfg):
         accelerator.prepare(G, D, optimizer_G, optimizer_D, train_loader)  # type: ignore
 
     # DEFINE LOSS
-    vanilla_gan_loss = VanillaGANLoss()
+    vanilla_gan_loss = VanillaGANLoss(discriminator=D)
 
     accelerator.wait_for_everyone()
 
@@ -163,8 +164,7 @@ def train(args, cfg):
         X = _discard_labels(X).float()
         z = torch.randn((X.shape[0], cfg.G.params.z_dim), device=device)
         fake = G(z).detach()
-        realscore, fakescore = D(X), D(fake)
-        loss = vanilla_gan_loss.forward_D(fakescore, realscore)
+        loss = vanilla_gan_loss.forward_D(fake, X)
         accelerator.backward(loss)
         optimizer_D.step()
         return dict(loss_D=loss.item(), lr_D=optimizer_D.param_groups[0]['lr'])
@@ -174,8 +174,7 @@ def train(args, cfg):
         X = _discard_labels(X).float()
         z = torch.randn((X.shape[0], cfg.G.params.z_dim), device=device)
         fake = G(z)
-        fakescore = D(fake)
-        loss = vanilla_gan_loss.forward_G(fakescore)
+        loss = vanilla_gan_loss.forward_G(fake)
         accelerator.backward(loss)
         optimizer_G.step()
         return dict(loss_G=loss.item(), lr_G=optimizer_G.param_groups[0]['lr'])

train_wgan_gp.py
@@ -12,7 +12,7 @@ from torchvision.utils import save_image
 
 import accelerate
 
-from losses import VanillaGANLoss
+from losses import WGANLoss
 from utils.logger import StatusTracker, get_logger
 from utils.data import get_dataset, get_data_generator
 from utils.misc import get_time_str, create_exp_dir, check_freq, find_resume_checkpoint, instantiate_from_config
@@ -151,7 +151,7 @@ def train(args, cfg):
         accelerator.prepare(G, D, optimizer_G, optimizer_D, train_loader)  # type: ignore
 
     # DEFINE LOSS
-    vanilla_gan_loss = VanillaGANLoss(discriminator=D)
+    wgan_loss = WGANLoss(discriminator=D, lambda_gp=cfg.train.lambda_gp)
 
     accelerator.wait_for_everyone()
 
@@ -165,7 +165,7 @@ def train(args, cfg):
         X = _discard_labels(X).float()
         z = torch.randn((X.shape[0], cfg.G.params.z_dim), device=device)
         fake = G(z).detach()
-        loss = vanilla_gan_loss.forward_D(fake, X)
+        loss = wgan_loss.forward_D(fake, X.reshape(fake.shape))
         accelerator.backward(loss)
         optimizer_D.step()
         return dict(loss_D=loss.item(), lr_D=optimizer_D.param_groups[0]['lr'])
@@ -175,7 +175,7 @@ def train(args, cfg):
         X = _discard_labels(X).float()
         z = torch.randn((X.shape[0], cfg.G.params.z_dim), device=device)
         fake = G(z)
-        loss = vanilla_gan_loss.forward_G(fake)
+        loss = wgan_loss.forward_G(fake)
         accelerator.backward(loss)
         optimizer_G.step()
         return dict(loss_G=loss.item(), lr_G=optimizer_G.param_groups[0]['lr'])